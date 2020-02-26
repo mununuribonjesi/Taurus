69a002a3c29f2fbc76015b9ec3c620911f2c8755
Munibanks <mbonjesi@gmail.com>
2020-02-26 17:12:25 +0000

    changes to package.json
    pushing project ups

--- server.js
+++ server.js
@@ -4,15 +4,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
 
-  res.json(customers);
-});
+
+
 
 app.set("port", process.env.PORT || 5000);
 
