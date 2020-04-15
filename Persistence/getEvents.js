b5b79a7b5548cd8ab0a215be5cd230bfa3d2d074
Munibanks <mbonjesi@gmail.com>
2020-04-15 18:36:32 +0100

    Update getEvents.js

--- Persistence/getEvents.js
+++ Persistence/getEvents.js
@@ -2,14 +2,9 @@
 
 var events = require('../ServiceLayer/eventsService.js');
 
-const fetch = require("node-fetch")
-
 const getEvents = async (req,res) => 
 {
-
     events.eventsService(req,res)
-  
-
 };
   
 module.exports ={
