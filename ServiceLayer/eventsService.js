ba12674d3a1b1027beb70c5f497e20dc0e691d1f
Muni Banks <33324169+mununuribonjesi@users.noreply.github.com>
2020-06-19 22:58:26 +0100

    remove console.log in server code
    removed console.log

--- ServiceLayer/eventsService.js
+++ ServiceLayer/eventsService.js
@@ -1,36 +1,33 @@
-"use strict"
-const fetch = require("node-fetch")
+"use strict";
+const fetch = require("node-fetch");
 
-async function eventsService(req,res)
-{
+async function eventsService(req, res) {
+  var latitude = parseInt(req.query.latitude);
+  var longitude = parseInt(req.query.longitude);
 
-var latitude = parseInt(req.query.latitude);
-var longitude = parseInt(req.query.longitude);
+  var url = new URL("http://www.skiddle.com/api/v1/events/search/"),
+    params = {
+      api_key: "1981a0231405eeba6bbbdd38829c8501",
+      latitude: latitude,
+      longitude: longitude,
+      limit: 30,
+      radius: 10,
+      eventcode: "CLUB",
+      order: "date",
+      description: 1,
+    };
 
-console.log(req.query.latitude);
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
 
-var url = new URL("http://www.skiddle.com/api/v1/events/search/"),
-params = {
-    api_key:'1981a0231405eeba6bbbdd38829c8501',
-    latitude:latitude, 
-    longitude:longitude,
-    limit:30,
-    radius:10,
-    eventcode:'CLUB',
-    order: 'date',
-    description:1
-}
-
-Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-   
-try {
-  const response = await fetch(url);
-  const doc = await response.json();
-  console.log(doc);
-  return res.send(doc);
-} catch (error) {
-  console.log(error);
-}
+  try {
+    const response = await fetch(url);
+    const doc = await response.json();
+    return res.send(doc);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 exports.eventsService = eventsService;
